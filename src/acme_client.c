7b4714dc5fae14acb43650fd5326466b92574a35
pyama86 <pyama@pepabo.com>
2017-04-23 19:30:02 +0900

    add init x509name

--- src/acme_client.c
+++ src/acme_client.c
@@ -7,6 +7,7 @@ void mrb_init_ossl_bn(mrb_state *mrb);
 void mrb_init_ossl_x509(mrb_state *mrb);
 void mrb_init_ossl_x509_request(mrb_state *mrb);
 void mrb_init_ossl_asn1(mrb_state *mrb);
+void mrb_init_ossl_x509_name(mrb_state *mrb);
 #define DONE mrb_gc_arena_restore(mrb, 0)
 
 void mrb_mruby_acme_client_gem_init(mrb_state *mrb)
@@ -26,6 +27,8 @@ void mrb_mruby_acme_client_gem_init(mrb_state *mrb)
   mrb_init_ossl_x509_request(mrb);
   DONE;
   mrb_init_ossl_asn1(mrb);
+  DONE;
+  mrb_init_ossl_x509_name(mrb);
   DONE;
 }
 void mrb_mruby_acme_client_gem_final(mrb_state *mrb)
