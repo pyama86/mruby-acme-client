b3424fd84191cdfea3702e8383554e8c789c8d43
pyama86 <pyama@pepabo.com>
2017-05-16 12:10:53 +0900

    attr to data_ptr

--- src/ossl_x509attr.h
+++ src/ossl_x509attr.h
@@ -2,13 +2,11 @@
     if (!(attr)) { \
       mrb_raise((mrb), E_RUNTIME_ERROR, "ATTR wasn't initialized!");                               \
     } \
-    mrb_iv_set((mrb), (obj), mrb_intern_lit(mrb, "x509attr"),                                          \
-               mrb_obj_value(                                                                      \
-                   Data_Wrap_Struct(mrb, mrb->object_class, &ossl_x509attr_type, (void *)attr)));  \
+    DATA_PTR(obj) = attr; \
+    DATA_TYPE(obj) = &ossl_x509attr_type; \
 } while (0)
 #define GetX509Attr(obj, attr) do { \
-    mrb_value value_attr = mrb_iv_get(mrb, obj, mrb_intern_lit(mrb, "x509attr"));                      \
-    attr = DATA_PTR(value_attr);                                                                   \
+    attr = DATA_PTR(obj);                                                                   \
     if (!(attr)) { \
       mrb_raise((mrb), E_RUNTIME_ERROR, "ATTR wasn't initialized!");                               \
     } \
