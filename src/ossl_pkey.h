5557f2143bc578cc734076c69043073874c2f496
pyama86 <pyama@pepabo.com>
2017-05-16 12:04:34 +0900

    rsa to data_ptr

--- src/ossl_pkey.h
+++ src/ossl_pkey.h
@@ -9,32 +9,29 @@ mrb_value ossl_pkey_alloc(mrb_state *mrb, mrb_value klass);
 EVP_PKEY *GetPKeyPtr(mrb_state *mrb, mrb_value obj);
 EVP_PKEY *GetPrivPKeyPtr(mrb_state *mrb, VALUE obj);
 
-#define NewPKey(klass)                                                                         \
-  mrb_obj_value(Data_Wrap_Struct(mrb, klass, &ossl_evp_pkey_type, 0))
+#define NewPKey(klass) mrb_obj_value(Data_Wrap_Struct(mrb, klass, &ossl_evp_pkey_type, 0))
 
-#define OSSL_PKEY_SET_PUBLIC(obj)                                                             \
+#define OSSL_PKEY_SET_PUBLIC(obj)                                                                  \
   mrb_iv_set(mrb, (obj), mrb_intern_lit(mrb, "private"), mrb_false_value())
 
-#define SetPKey(obj, pkey)                                                                    \
+#define SetPKey(obj, pkey)                                                                         \
   do {                                                                                             \
     if (!(pkey)) {                                                                                 \
       mrb_raise((mrb), E_RUNTIME_ERROR, "PKEY wasn't initialized!");                               \
     }                                                                                              \
-    mrb_iv_set((mrb), (obj), mrb_intern_lit(mrb, "pkey"),                                          \
-               mrb_obj_value(                                                                      \
-                   Data_Wrap_Struct(mrb, mrb->object_class, &ossl_evp_pkey_type, (void *)pkey)));  \
-    OSSL_PKEY_SET_PUBLIC(obj);                                                                \
+    DATA_PTR(obj) = pkey;                                                                          \
+    DATA_TYPE(obj) = &ossl_evp_pkey_type;                                                          \
+    OSSL_PKEY_SET_PUBLIC(obj);                                                                     \
   } while (0)
 
-#define GetPKey(obj, pkey)                                                                    \
+#define GetPKey(obj, pkey)                                                                         \
   do {                                                                                             \
-    mrb_value value_pkey = mrb_iv_get(mrb, obj, mrb_intern_lit(mrb, "pkey"));                      \
-    pkey = DATA_PTR(value_pkey);                                                                   \
+    pkey = DATA_PTR(obj);                                                                   \
   } while (0)
 
-#define SafeGetPKey(obj, pkey)                                                                \
+#define SafeGetPKey(obj, pkey)                                                                     \
   do {                                                                                             \
-    OSSL_Check_Kind(mrb, (obj), cPKey);                                                          \
-    GetPKey((obj), (pkey));                                                                \
+    OSSL_Check_Kind(mrb, (obj), cPKey);                                                            \
+    GetPKey((obj), (pkey));                                                                        \
   } while (0)
 #endif /* _OSSL_PKEY_H_ */
